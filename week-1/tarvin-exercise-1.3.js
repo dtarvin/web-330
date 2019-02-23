0710d2be3d528f0e81fcda0b364ab1f607f8baf2
David Tarvin <david.tarvin@physiciansmutual.com>
2019-02-22 20:07:41 -0600

    delete starter code from exercise 1.3

--- week-1/tarvin-exercise-1.3.js
+++ week-1/tarvin-exercise-1.3.js
@@ -38,27 +38,3 @@ function CellPhone(manufacturer, model, color, price) {
 var iPhone = new CellPhone("Apple", "iPhone XR", "Space Gray", "$799.99");
 
 iPhone.getDetails();
-
-/*
- Expected output:
-
- FirstName LastName
- <AssignmentName>
- <Today's Date>
-
- -- DISPLAYING CELL PHONE DETAILS --
- Manufacturer: <manufacturer>
- Model: <model>
- Color: <color>
- Price: <price>
-
-
-*/
-
-// start program
-
-
-
-
-
-// end program
